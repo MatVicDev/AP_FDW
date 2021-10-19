708b8849c0c191115b843164bed30727b869a6e7
MatVicDev <matheus@teste.com>
2021-10-19 15:24:27 -0300

    Criando o arquivo.ts e importando no componente principal

---
+++ src/app/arquivo.ts
@@ -0,0 +1,32 @@
+export const agenda = [
+    {
+        nome: 'Matheus Victor',
+        ru: '3279722',
+        curso: 'Análise e desenvolvimento de sistema',
+        dt_nasc: '1999'
+    },
+    {
+        nome: 'Anatoly Karpov',
+        ru: '1234567',
+        curso: 'Economia',
+        dt_nasc: '1951'
+    },
+    {
+        nome: 'Robert James Fischer',
+        ru: '0987654',
+        curso: 'Engenheiro de Software',
+        dt_nasc: '1943'
+    },
+    {
+        nome: 'Garry Kasparov',
+        ru: '1235670',
+        curso: 'Engenharia civil',
+        dt_nasc: '1963'
+    },
+    {
+        nome: 'Magnus Carlsen',
+        ru: '1122334',
+        curso: 'Contabilidade',
+        dt_nasc: '1990'
+    }
+]
